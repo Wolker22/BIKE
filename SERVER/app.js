fbf806143c57172673af4c561e5f4576ea23f8b8
Wolker22 <139255844+Wolker22@users.noreply.github.com>
2024-05-30 01:23:44 +0200

    BUENAS NOCHES

--- SERVER/app.js
+++ SERVER/app.js
@@ -88,7 +88,7 @@ let clients = {};
       res.sendStatus(200);
     });
 
-    const PORT = 443;
+    const PORT = process.env.PORT || 3000;
     server.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
     });
