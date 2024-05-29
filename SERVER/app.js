06c8db481fe9d436b54ae0fe7fb287830317dee7
Wolker22 <139255844+Wolker22@users.noreply.github.com>
2024-05-29 23:57:28 +0200

    REQUI

--- SERVER/app.js
+++ SERVER/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const https = require("https");
 const fs = require("fs");
+const cors = require('cors');
 const WebSocket = require("ws");
 const path = require("path");
 const locationsRouter = require("./routes/locations");
