9b2fc6cffc9f217e912d74090bcc0a3811698a43
Wolker22 <139255844+Wolker22@users.noreply.github.com>
2024-05-31 22:12:41 +0200

    Update main.js

--- client/main.js
+++ client/main.js
@@ -272,8 +272,6 @@ function drawGeofence(data) {
     strokeColor: "#FF0000",
     strokeOpacity: 0.8,
     strokeWeight: 2,
-    fillColor: "#FF0000",
-    fillOpacity: 0.35
   });
 
   geofencePolygon.setMap(map);
